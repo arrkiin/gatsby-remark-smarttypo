6ac293b6dbabfe6ba9c06bcc4464157d1e0cc873
arrkiin <arrkiin@gmail.com>
2018-06-08 10:32:01 +0200

    Additional unicode characters

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const visit = require(`unist-util-visit`);
 
-var SCOPED_ABBR_RE = /\((c|tm|r|p)\)/ig;
 var SCOPED_ABBR = {
   c: '©',
   r: '®',
   p: '§',
-  tm: '™'
+  tm: '™',
+  '1/2':'½',
+  '1/4':'¼',
+  '3/4':'¾',
+  '*':'×',
+  '/':'÷',
+  '+-':'±',
+  'deg':'°',
+  '^1':'¹',
+  '^2':'²',
+  '^3':'³',
+  'my':'µ'
 };
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+var SCOPED_ABBR_RE = RegExp('\\((' + Object.keys(SCOPED_ABBR).map(key => escapeRegExp(key)).join('|') + ')\\)','g')
 
 function replaceFn(match, name) {
   return SCOPED_ABBR[name.toLowerCase()];
